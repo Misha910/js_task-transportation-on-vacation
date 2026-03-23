2e9ee2ece14c771f88e2ba606a4301abcf05095f
Misha Pysaniuk <pusaniuk.m10@gmail.com>
2026-03-23 23:09:05 +0100

    Fix rental cost calculation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,23 @@
  *
  * @return {number}
  */
+
 function calculateRentalCost(days) {
-  // write code here
+  const preicOneDay = 40;
+  const bigDiscount = 50;
+  const somoulDiscount = 20;
+
+  if (days >= 7) {
+    return days * preicOneDay - bigDiscount;
+  }
+
+  if (days >= 3 && days < 7) {
+    return days * preicOneDay - somoulDiscount; // write code here
+  }
+
+  if (days < 3) {
+    return days * preicOneDay;
+  }
 }
 
 module.exports = calculateRentalCost;
